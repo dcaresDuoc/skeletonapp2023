24ae4599f7e8df3dd64800dc443258d44a02fa29
dcaresDuoc <d.caresg@profesor.duoc.cl>
2023-08-18 11:19:22 -0400

    add new code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  userHome: any;
+  value = "dcaresg";
+
+
+  constructor(private activeroute: ActivatedRoute, private router: Router) {
+    // if (history.state.user) {
+    //   this.userHome = history.state.user;      
+    // }
+    
+    this.activeroute.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation()?.extras.state) {
+          this.userHome = this.router.getCurrentNavigation()?.extras.state?.['user'];
+      }
+  });
+
+    
+
+  }
+
+  ngOnInit(): void {
+   
+  }
 
 }
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup,Validators   } from '@angular/forms';
+import { Router,NavigationExtras } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  textBtn = "INGRESAR";
+  textUser = "Usuario";
+  textPass = "Contraseña";
+  desUser = "ingrese sadfghajskdfgbhmfghjekrltghjmghjkl";
+  desPass = "ingrese contraseña";
+
+  user={
+    usuario:"asdfghj",
+    apellido:"Dicaprio"
+  }
+
+    usuario = new FormGroup({
+    user: new FormControl('',[Validators.required, Validators.minLength(4),Validators.maxLength(8)]),
+    pass: new FormControl('',[Validators.required, Validators.minLength(4),Validators.maxLength(4)]),
+  });
+
+
+  goToPagina2() {
+    let navigationExtras: NavigationExtras = {
+      state: {user: this.usuario.value.user}
+      };
+      this.router.navigate(['/home'],navigationExtras); // Esta linea es la que me permite navegar a otro page 
+  }
+
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+}
